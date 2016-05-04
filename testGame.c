0c68272f11d348d3e4b6d056c533026a3bdd37ae
Richard Guan <richardguan1@msn.com>
2016-05-04 18:23:48 +1000

    Added files via upload

---
+++ testGame.c
@@ -0,0 +1,8 @@
+//name: Richard Guan, Grant Wei, Raihan Zhafranata
+//date: 4/5/2016
+//tests to make sure game is not broken
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Game.h"
