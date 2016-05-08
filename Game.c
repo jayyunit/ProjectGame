e0dbd38f7eb16e330378ae7a92000a7a2cf511ec
Richard Guan <richardguan1@msn.com>
2016-05-08 18:00:30 +1000

    Added files via upload

---
+++ Game.c
@@ -0,0 +1,28 @@
+//name: Richard Guan, Grant Wei, Raihan Zhafranata
+//date: 5/5/2016
+//this program will play the game
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Game.h"
+
+
+typedef struct _game * Game {
+    int currentTurn; //saves the current turn of the game
+    int currentPlayer; //saves the current player
+    int regionID; //ID of each hexagon tiles
+    int vertex; //co-ordinates of each hexagon tile's corner from 0 - 5
+    int axis;
+} game;
+
+typedef struct _regionID {
+    int disciplineCode; //type of students produced
+    int diceValue; // dice value to produce students
+    int edge; //edges for ARCs
+} regionID;
+
+typedef struct _axis {
+    int x; //x co-ordinates
+    int y; //y co-ordinates
+} axis;
